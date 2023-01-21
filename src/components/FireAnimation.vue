d23a593c809dd1f09cdc204a281b68dfde20ea07
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-21 13:10:30 -0300

    Crear FireAnimation

---
+++ src/components/FireAnimation.vue
@@ -0,0 +1,12 @@
+<template>
+  <img
+    src="../assets/fire-1.gif"
+    alt=""
+    class="absolute -top-36 -left-24 rotate-135"
+  />
+  <img
+    src="../assets/fire-1.gif"
+    alt=""
+    class="absolute -top-36 -right-24 -rotate-135"
+  />
+</template>
