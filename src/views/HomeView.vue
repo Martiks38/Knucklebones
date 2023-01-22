082a8fbc6941757e951266cfdb6caa493f9fe0f3
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-22 19:05:02 -0300

    Modificar HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,19 @@
 <script setup lang="ts">
+import FireAnimation from "@/components/FireAnimation.vue";
+import GenericButton from "@/components/GenericButton.vue";
+
 const linkButtons = [
   ["Jugar Local", "/local"],
   ["Jugar Multijugador", "/multiplayer"],
   ["Cómo jugar", "/how_to_play"],
 ];
-
-const activeHover = (ev: MouseEvent) => {
-  const optionsButtonsHome = Array.from(
-    document.querySelectorAll(".styleButtonMenu")
-  );
-  const buttonTarget = (ev.currentTarget as HTMLElement).dataset
-    .option as string;
-
-  optionsButtonsHome.forEach((button, index) => {
-    if (index === parseInt(buttonTarget)) {
-      button.classList.add("active");
-    } else {
-      button.classList.remove("active");
-    }
-  });
-};
 </script>
 
 <template>
   <main
     class="relative grid place-content-center w-full h-screen bg-[#000] overflow-hidden"
   >
-    <img
-      src="../assets/fire-1.gif"
-      alt=""
-      class="absolute -top-36 -left-24 rotate-135"
-    />
-    <img
-      src="../assets/fire-1.gif"
-      alt=""
-      class="absolute -top-36 -right-24 -rotate-135"
-    />
+    <FireAnimation />
     <div class="w-[860px]">
       <div class="mb-12 text-white">
         <div></div>
@@ -48,53 +26,16 @@ const activeHover = (ev: MouseEvent) => {
       <div
         class="flex flex-col items-center gap-4 mx-auto max-w-max text-white"
       >
-        <div
+        <GenericButton
           v-for="(link, index) in linkButtons"
           :key="index"
-          class="relative w-full"
+          :href="link[1]"
+          :option-number="index"
+          :styles="index === 0 ? 'active' : ''"
         >
-          <div
-            class="absolute top-0 left-0 h-full w-full outline outline-2 outline-black outline-offset-[-8px] styleButtonMenu"
-            :class="index === 0 ? 'active' : ''"
-          ></div>
-          <router-link
-            @mouseenter="activeHover"
-            :to="link[1]"
-            :key="index"
-            class="relative block px-16 py-4 text-center font-bold"
-            :data-option="index"
-            >{{ link[0] }}</router-link
-          >
-        </div>
+          {{ link[0] }}
+        </GenericButton>
       </div>
     </div>
   </main>
 </template>
-
-<style scope>
-.active {
-  background-color: #fc0403;
-}
-
-.styleButtonMenu {
-  clip-path: polygon(0 0, 100% 0, 84% 50%, 100% 100%, 0 100%, 16% 50%);
-  animation: moveBackground 800ms linear 0s alternate infinite none;
-}
-
-@keyframes moveBackground {
-  0% {
-    top: 0px;
-    left: 0px;
-  }
-  100% {
-    top: 2px;
-    left: 2px;
-  }
-}
-
-@media (prefers-reduced-motion: reduce) {
-  .styleButtonMenu {
-    animation-name: none;
-  }
-}
-</style>
--- src/views/__tests__/HomeView.test.tsx
+++ src/views/__tests__/HomeView.test.tsx
@@ -1,19 +1,33 @@
-import { describe, expect, it } from "vitest";
-import { fireEvent, render, screen } from "@testing-library/vue";
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  type RenderResult,
+} from "@testing-library/vue";
 
 import HomeView from "../HomeView.vue";
 
-let content: Element;
-
 describe("<HomeView />", () => {
-  content = render(HomeView).container;
+  let renderResult: RenderResult;
+
+  beforeEach(() => {
+    renderResult = render(HomeView);
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
 
   it("Correctly render", () => {
     expect(screen.getByText("Matatena")).toBeDefined();
   });
 
   it("A only button have the active class", () => {
-    const bgButtons = content.querySelectorAll(".styleButtonMenu");
+    const { container } = renderResult;
+
+    const bgButtons = container.querySelectorAll(".styleButton");
 
     const cantActive = Array.from(bgButtons).reduce((cur, val) => {
       return val.classList.contains("active") ? cur + 1 : cur;
@@ -22,24 +36,28 @@ describe("<HomeView />", () => {
     expect(cantActive).toEqual(1);
   });
 
-  it("Hovering the mouse over a button adds the active class.", () => {
-    const buttons = content.querySelectorAll("[data-option]");
+  it("Hovering the mouse over a button adds the active class.", async () => {
+    const { container } = renderResult;
 
-    fireEvent.mouseEnter(buttons[1]);
+    const buttons = container.querySelectorAll("[data-option]");
 
-    const bgButtons = content.querySelectorAll(".styleButtonMenu");
+    await fireEvent.mouseEnter(buttons[1]);
+
+    const bgButtons = container.querySelectorAll(".styleButton");
 
     const listClasses = Array.from(bgButtons[1].classList);
-
+    screen.debug();
     expect(listClasses).toContain("active");
   });
 
-  it("When hovering over a button, there should only be one active class.", () => {
-    const buttons = content.querySelectorAll("[data-option]");
+  it("When hovering over a button, there should only be one active class.", async () => {
+    const { container } = renderResult;
 
-    fireEvent.mouseEnter(buttons[1]);
+    const buttons = container.querySelectorAll("[data-option]");
 
-    const bgButtons = Array.from(content.querySelectorAll(".styleButtonMenu"));
+    await fireEvent.mouseEnter(buttons[1]);
+
+    const bgButtons = Array.from(container.querySelectorAll(".styleButton"));
 
     const cantActiveClass = bgButtons.reduce((cur, val) => {
       return val.classList.contains("active") ? cur + 1 : cur;
