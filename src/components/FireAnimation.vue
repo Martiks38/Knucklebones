b45680726677205f86f77338e22241664e3f77ea
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-22 19:04:20 -0300

    Crear FireAnimation y GenericButton

--- src/components/FireAnimation.vue
+++ src/components/FireAnimation.vue
@@ -1,12 +1,14 @@
 <template>
-  <img
-    src="../assets/fire-1.gif"
-    alt=""
-    class="absolute -top-36 -left-24 rotate-135"
-  />
-  <img
-    src="../assets/fire-1.gif"
-    alt=""
-    class="absolute -top-36 -right-24 -rotate-135"
-  />
+  <div class="absolute w-screen">
+    <img
+      src="../assets/fire-1.gif"
+      alt=""
+      class="absolute -top-36 -left-24 rotate-135"
+    />
+    <img
+      src="../assets/fire-1.gif"
+      alt=""
+      class="absolute -top-36 -right-24 -rotate-135"
+    />
+  </div>
 </template>
