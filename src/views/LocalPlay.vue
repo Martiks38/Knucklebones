6e6fa2258c856f40e49d90a0efcc243f013268be
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-09 15:13:06 -0300

    Añdir detección de fin de partida

--- src/views/LocalPlay.vue
+++ src/views/LocalPlay.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
-import { onBeforeMount, provide, reactive } from 'vue'
+import { onBeforeMount, provide, reactive, ref } from 'vue'
 import PlayerPanel from '@/components/PlayerPanel.vue'
 import type { Player } from '@/typings/index'
+
+type GameState = 'playing' | 'finished'
 
 const player1: Player = reactive({
 	number: '1',
@@ -15,6 +17,8 @@ const player2: Player = reactive({
 
 const informationNewDice = reactive({ col: 0, value: 0 })
 const players: [Player, Player] = [player1, player2]
+
+const gameState = ref<GameState>('playing')
 
 const changeNewDice = (
 	currentDice: { col: number; value: number },
@@ -34,7 +38,17 @@ onBeforeMount(() => {
 
 <template>
 	<main class="flex flex-col-reverse justify-between w-[1024px] mx-auto h-screen py-4 text-white">
-		<PlayerPanel :player="player1" :players="players" />
-		<PlayerPanel :player="player2" :players="players" :class="'flex-row-reverse items-start'" />
+		<PlayerPanel
+			:player="player1"
+			:players="players"
+			@finished-game="() => (gameState = 'finished')"
+		/>
+		<PlayerPanel
+			:player="player2"
+			:players="players"
+			:class="'flex-row-reverse items-start'"
+			@finished-game="() => (gameState = 'finished')"
+		/>
+		<div v-if="gameState === 'finished'">Finished!!!</div>
 	</main>
 </template>
