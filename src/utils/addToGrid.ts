b31cb11fd581e2373ffb2537fce2cc993dd5b9e0
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-09 00:14:59 -0300

    Modificar addToGrid para detectar otros dados

--- src/utils/addToGrid.ts
+++ src/utils/addToGrid.ts
@@ -1,3 +1,5 @@
+import type { GridColumn } from '@/typings'
+
 export function addToGrid<T>({
 	colNumber,
 	grid,
@@ -6,14 +8,37 @@ export function addToGrid<T>({
 	value: T
 	colNumber: number
 	grid: {
-		col: {
-			value: T
-			id: string
-		}[]
+		col: GridColumn<T>[]
 		id: string
 	}[]
 }): void {
-	const slotDice = grid[colNumber].col.find((cell) => cell?.value === 0)
+	const diceCol = grid[colNumber].col.slice()
+	const diceSlot = diceCol.find((cell) => cell?.value === 0)
 
-	if (slotDice) slotDice.value = value
+	if (!diceSlot) return
+
+	diceSlot.value = value
+
+	const dices = diceCol.map((dice) => dice.value).filter((value) => value !== 0)
+	const cantDices = dices.length
+	const numberNonReatingValues = new Set(dices).size
+
+	if (cantDices === numberNonReatingValues) return
+	if (cantDices - numberNonReatingValues === 2) {
+		diceCol.forEach((dice) => (dice.factor = 3))
+
+		grid[colNumber].col = diceCol
+		return
+	}
+
+	dices.forEach((value, ind) => {
+		const otherPosition = dices.lastIndexOf(value)
+
+		if (otherPosition !== ind) {
+			diceCol[ind].factor = 2
+			diceCol[otherPosition].factor = 2
+		}
+	})
+
+	grid[colNumber].col = diceCol
 }
