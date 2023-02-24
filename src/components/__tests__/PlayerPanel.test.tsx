4dccccc1013dda930b2c02d9cf12b80996a78d80
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-24 17:10:32 -0300

    Añadir test a PlayerPanel

--- src/components/__tests__/PlayerPanel.test.tsx
+++ src/components/__tests__/PlayerPanel.test.tsx
@@ -55,4 +55,20 @@ describe('<PlayerPanel />', () => {
 		expect(newHighlightColumn?.getAttribute('class')).toContain('highlight')
 		expect(container.querySelectorAll('.highlight')).toHaveLength(1)
 	})
+
+	it("At the start of the game, only the player's zero score should be on the screen. The zero score of the dice columns must not appear.", () => {
+		const { queryAllByText } = Panel
+
+		expect(queryAllByText(/0/g)).toHaveLength(1)
+	})
+
+	it('Adding a new die to a column displays the total score for the column', async () => {
+		const { container } = Panel
+
+		const highlightCol = container.querySelector('.highlight') as HTMLElement
+
+		await fireEvent.click(highlightCol)
+
+		expect(Panel.queryByText(/[1-6]/g)).toBeDefined()
+	})
 })
