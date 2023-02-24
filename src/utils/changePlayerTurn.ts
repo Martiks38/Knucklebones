4ea2d8f0343d98a18146cb652318837b76717f49
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-24 15:45:44 -0300

    Borrar changePlayerTurn

--- src/utils/changePlayerTurn.ts
+++
@@ -1,6 +0,0 @@
-import type { Player } from '@/typings'
-
-export function changePlayerTurn(player1: Player, player2: Player) {
-	player1.isTurn = !player1.isTurn
-	player2.isTurn = !player2.isTurn
-}
