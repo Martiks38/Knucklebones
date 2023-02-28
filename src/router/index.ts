a58f953a38bf8df38db5c6e06129ebbcf9d12405
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-27 21:34:56 -0300

    Modificar main.ts y router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LocalPlay from '../views/LocalPlay.vue'
-import NotFound from '../views/NotFound.vue'
+
+const routes = [
+	{
+		path: '/',
+		name: 'home',
+		component: HomeView
+	},
+	{
+		path: '/how_to_play',
+		name: 'howToPlay',
+		component: () => import('@/views/HowToPlayView.vue')
+	},
+	{
+		path: '/local_play',
+		name: 'playLocal',
+		component: () => import('@/views/LocalPlay.vue')
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: () => import('@/views/NotFound.vue')
+	}
+]
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView
-		},
-		{
-			path: '/how_to_play',
-			name: 'howToPlay',
-			component: () => import('@/views/HowToPlayView.vue')
-		},
-		{
-			path: '/local_play',
-			name: 'playLocal',
-			component: LocalPlay
-		},
-		{
-			path: '/:pathMatch(.*)*',
-			name: 'NotFound',
-			component: NotFound
-		}
-	]
+	history: createWebHistory(),
+	routes
 })
 
 export default router
