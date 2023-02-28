d67339633024b997e3d6ec1c426e5aecfbc40b9f
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-28 11:39:15 -0300

    Añadir config vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 /// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		splitVendorChunkPlugin(),
 		vue({
 			template: {
 				transformAssetUrls: {
